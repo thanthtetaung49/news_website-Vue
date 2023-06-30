4e45919d5f768a964781d39189eb26d646db67fa
thanthtetaung49 <thanthtetaung12@gmail.com>
2023-06-30 18:50:27 +0630

    first commit

--- src/views/NewsDetail.vue
+++ src/views/NewsDetail.vue
@@ -87,88 +87,12 @@
                   </ul>
                 </div>
                 <div class="section-title d-flex">
-                  <h3 class="mr-20">View count:</h3>
+                  <h3 class="mr-20">Viewer:</h3>
                   <span class="p-0 mt-2">
                     <i class="fa-solid fa-eye mr-2"></i
                     >{{ postViewCount }} views</span
                   >
                 </div>
-              </div>
-            </div>
-            <!-- From -->
-            <div class="row">
-              <div class="col-lg-8">
-                <form
-                  class="form-contact contact_form mb-80"
-                  action="contact_process.php"
-                  method="post"
-                  id="contactForm"
-                  novalidate="novalidate"
-                >
-                  <div class="row">
-                    <div class="col-12">
-                      <div class="form-group">
-                        <textarea
-                          class="form-control w-100 error"
-                          name="message"
-                          id="message"
-                          cols="30"
-                          rows="9"
-                          onfocus="this.placeholder = ''"
-                          onblur="this.placeholder = 'Enter Message'"
-                          placeholder="Enter Message"
-                        ></textarea>
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="form-group">
-                        <input
-                          class="form-control error"
-                          name="name"
-                          id="name"
-                          type="text"
-                          onfocus="this.placeholder = ''"
-                          onblur="this.placeholder = 'Enter your name'"
-                          placeholder="Enter your name"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-sm-6">
-                      <div class="form-group">
-                        <input
-                          class="form-control error"
-                          name="email"
-                          id="email"
-                          type="email"
-                          onfocus="this.placeholder = ''"
-                          onblur="this.placeholder = 'Enter email address'"
-                          placeholder="Email"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-12">
-                      <div class="form-group">
-                        <input
-                          class="form-control error"
-                          name="subject"
-                          id="subject"
-                          type="text"
-                          onfocus="this.placeholder = ''"
-                          onblur="this.placeholder = 'Enter Subject'"
-                          placeholder="Enter Subject"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div class="form-group mt-3">
-                    <button
-                      type="submit"
-                      class="button button-contactForm boxed-btn"
-                    >
-                      Send
-                    </button>
-                  </div>
-                </form>
               </div>
             </div>
           </div>
